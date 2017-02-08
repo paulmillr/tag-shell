db9ffb9a4da742d281775e74e2e9f0e04717dba9
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-02-08 18:48:02 +0200

    add parsing tests

--- test.js
+++ test.js
@@ -5,29 +5,43 @@ describe('tag-shell', () => {
   tagShell.should.be.a('function');
   tagShell.should.itself.respondTo('makeTag');
 
-  describe('should parse args correctly', () => {
+  describe('parses arguments correctly', () => {
     const sh = tagShell.makeTag(args => args);
 
-    it('splits literal part', () => {
+    it('splits literal part by spaces', () => {
       sh``.should.eql([]);
-      sh`echo`.should.eql(['echo']);
-      sh`echo hello`.should.eql(['echo', 'hello']);
-      sh`echo hello world`.should.eql(['echo', 'hello', 'world']);
+      sh`cmd`.should.eql(['cmd']);
+      sh`cmd hello`.should.eql(['cmd', 'hello']);
+      sh`cmd hello world`.should.eql(['cmd', 'hello', 'world']);
     });
 
-    it('handles all whitespaces', () => {
-      sh`echo\ttab`.should.eql(['echo', 'tab']);
-      sh`echo  new\nline`.should.eql(['echo', 'new', 'line']);
+    it('takes in account all whitespaces', () => {
+      sh`cmd\ttab`.should.eql(['cmd', 'tab']);
+      sh`cmd  new\nline`.should.eql(['cmd', 'new', 'line']);
     });
 
-    it('appends substitutions', () => {
+    it('pushes subst if literal ends with space', () => {
       sh`${''}`.should.eql(['']);
-      sh`${'echo'}`.should.eql(['echo']);
+      sh`cmd ${''} empty`.should.eql(['cmd', '', 'empty']);
+      sh`cmd with\t${'tab'}`.should.eql(['cmd', 'with', 'tab']);
+      sh`cmd new\n${'line'}`.should.eql(['cmd', 'new', 'line']);
+    });
+
+    it(`appends subst to last arg if literal doesn't end with space`, () => {
+      sh`param=${''}`.should.eql(['param=']);
+      sh`param=${'value'} --bool`.should.eql(['param=value', '--bool']);
+      sh`param=${'with space'} --bool`.should.eql(['param=with space', '--bool']);
+    });
+
+    it(`doesn't coerce substs to strings`, () => {
+      sh`cmd ${1} ${false}`.should.eql(['cmd', 1, false]);
+      sh`cmd ${[]} ${true}`.should.eql(['cmd', [], true]);
     });
 
     it('skips `null` and `undefined`', () => {
-      sh`echo ${null}`.should.eql(['echo']);
-      sh`echo ${undefined}`.should.eql(['echo']);
+      sh`${null}`.should.eql([]);
+      sh`${null} --bool`.should.eql(['--bool']);
+      sh`cmd ${undefined}`.should.eql(['cmd']);
     });
   });
 });
